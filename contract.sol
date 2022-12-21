e161b64fe193ad07f35accf55a2e0ef7157c92eb
01110101 <42702073+alisherweb3@users.noreply.github.com>
2022-12-21 23:08:10 +0500

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -4,4 +4,43 @@ contract ZombieFactory {
     uint myUnsignedInteger = 100;
     uint dnaDigits = 16;
     uint dnaModuls = 10 ** dnaDigits;
+    
+    struct Zombie {
+        string name;
+        uint dna;
+        
+        
+    Zombie[] public zombies;    
+    
+    
+    
+    function _createZombie (string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+    }
+    
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+    
+/*    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked9_str)));
+        return rand & dnaModules;
+    }
+    
+*/    
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+    function createRandomZombie(string mmory _name) public {
+        uint randDna = _generateRandom(_name);
+        _createZombie(_name, randDna);
+    }
+
+
+
+
 }
