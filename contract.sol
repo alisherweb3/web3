0f92d8ed6cccdc1bb61a2f610380c220d31b38bd
01110101 <42702073+alisherweb3@users.noreply.github.com>
2022-12-21 23:16:26 +0500

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -12,12 +12,20 @@ contract ZombieFactory {
         
     Zombie[] public zombies;    
     
+    
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+    
      
     function _createZombie (string memory _name, uint _dna) private {
-        zombies.push(Zombie(_name, _dna));
+        zombies.push(Zombie(_name, _dna)) -1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        emit NewZombie(id, _name, _dna);
     }
     
     function _generateRandomDna(string memory _str) private view returns (uint) {
+        require(ownerZombieCount[msg.sender] == 0);
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
@@ -25,11 +33,6 @@ contract ZombieFactory {
 
     function createRandomZombie(string memory _name) public {
         uint randDna = _generateRandomDna(_name);
-        _createZombie(_name, randDna);
-    }
-
-    function createRandomZombie(string mmory _name) public {
-        uint randDna = _generateRandom(_name);
         _createZombie(_name, randDna);
     }
 
